9c78a9daa2d36016c0570621ec7df8f4b0a031e3
Lance Woodson <lance@webmaneuvers.com>
2012-04-25 09:33:24 -0700

    Fixing active record runtime dependency

--- lib/will_summarize.rb
+++ lib/will_summarize.rb
@@ -7,3 +7,32 @@ class ActiveRecord::Base
   extend WillSummarize::Summarizable
 end
 
+#begin
+#  require 'rails_admin/config/actions'
+#  require 'rails_admin/config/actions/base'
+#
+#  # custom action
+#  module RailsAdmin
+#    module Config
+#      module Actions
+#        class PopulateSummary < RailsAdmin::Config::Actions::Base
+#          register_instance_option :member? do
+#            true
+#          end
+#
+#          register_instance_option :controller do
+#            Proc.new do
+#              @object.populate_summary
+#              flash[:notice] = "Summary updated."
+#              redirect_to back_or_index
+#            end
+#          end
+#
+#          RailsAdmin::Config::Actions.register self
+#        end
+#      end
+#    end
+#  end
+#
+#rescue LoadError; end
+
